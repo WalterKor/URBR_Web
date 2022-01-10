72e9a9fa2df040d8e40c3ea8f75e55573abd1d04
Walter <78077569+WalterKor@users.noreply.github.com>
2022-01-10 21:53:36 +0900

    mongo connect

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb+srv://walter:everglow95@todoapp.hudxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, 
+}).then(()=> console.log('MongoDB Connected')).catch(err => console.log(err));
+
+
 
 app.get('/', (req, res)=> res.send('Hello world'));
 
